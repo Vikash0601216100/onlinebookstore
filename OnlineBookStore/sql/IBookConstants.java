5635d62d3799118f61fe086c09c79047d16391b7
Vikash0601216100 <95157112+Vikash0601216100@users.noreply.github.com>
2022-03-22 15:48:57 +0530

    Update IBookConstants.java

--- OnlineBookStore/sql/IBookConstants.java
+++ OnlineBookStore/sql/IBookConstants.java
@@ -4,7 +4,7 @@ public interface IBookConstants {
 	
     public static String TABLE_BOOK = "books";
 	
-	public static String COLUMN_NAME = "name";
+	public static String COLUMN_NAME = "name1";
 	public static String COLUMN_BARCODE = "barcode";
 	public static String COLUMN_AUTHOR = "author";
 	public static String COLUMN_PRICE = "price";
